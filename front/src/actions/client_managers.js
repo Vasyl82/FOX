e51823326109f06633202b94274ae44770d95635
Taraslysak <lysak.ipr@gmail.com>
2020-10-23 10:15:11 +0300

    changed api urls from 'client_managers' to 'managers'

--- front/src/actions/client_managers.js
+++ front/src/actions/client_managers.js
@@ -3,10 +3,10 @@ import { RepresentationService } from '../services'
 const repr = new RepresentationService
 const getClientManagerList = ({ role = null, params = null, additional = false, signal = null }) => {
     if (role === 'CliAdm') {
-        return repr.displayDeleteList('client_managers', params, additional, signal)
+        return repr.displayDeleteList('managers', params, additional, signal)
     }
     else {
-        return repr.displaySimpleList('client_managers', params, additional, signal)
+        return repr.displaySimpleList('managers', params, additional, signal)
     }
 }
 
