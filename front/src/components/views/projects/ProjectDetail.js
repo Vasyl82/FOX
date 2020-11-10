5b5010ca2850a453b8e813be6bd9c27b10ae0eb3
Taraslysak <lysak.ipr@gmail.com>
2020-11-10 16:02:17 +0200

    readonly contractor in project detail

--- front/src/components/views/projects/ProjectDetail.js
+++ front/src/components/views/projects/ProjectDetail.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import DjangoCSRFToken from 'django-react-csrftoken'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import DjangoCSRFToken from "django-react-csrftoken";
 import {
   CForm,
   CFormGroup,
   CInput,
-  CLabel, CRow,
+  CLabel,
+  CRow,
   CCol,
   CTextarea,
   CLink,
@@ -15,22 +16,26 @@ import {
   CCardBody,
   CCardHeader,
   CCardTitle,
-  CFormText
+  CFormText,
 } from "@coreui/react";
 
-import { getProfileFetch, getContractorList, setProjectId, clearList } from '../../../actions'
-import { FoxApiService } from '../../../services'
-import { ActivityLog } from '../../activity_log'
-import { FoxSwitchGroup } from '../../../utils'
-import { FoxReactSelectFormGroup } from '../../forms'
-import { permitOptions } from './optionsLists'
-import { WithLoading, WithLoadingSpinner, SubmitSpinner } from '../../loadings'
-import { handleError } from '../../errors'
+import {
+  getProfileFetch,
+  getContractorList,
+  setProjectId,
+  clearList,
+} from "../../../actions";
+import { FoxApiService } from "../../../services";
+import { ActivityLog } from "../../activity_log";
+import { FoxSwitchGroup } from "../../../utils";
+import { FoxReactSelectFormGroup } from "../../forms";
+import { permitOptions } from "./optionsLists";
+import { WithLoading, WithLoadingSpinner, SubmitSpinner } from "../../loadings";
+import { handleError } from "../../errors";
 
 const foxApi = new FoxApiService();
 
 class ProjectDetail extends Component {
-
   state = {
     name: "",
     location: "",
@@ -48,240 +53,272 @@ class ProjectDetail extends Component {
     work_alone: false,
     work_at_sensitive_area: false,
     cold_work: false,
-    error: false
-  }
+    error: false,
+  };
 
-  handleChange = event => {
+  handleChange = (event) => {
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
-  }
+  };
 
-  handleCheck = event => {
+  handleCheck = (event) => {
     this.setState({
-      [event.target.name]: event.target.checked
-    })
-  }
+      [event.target.name]: event.target.checked,
+    });
+  };
 
-  handleSubmit = async event => {
+  handleSubmit = async (event) => {
     event.preventDefault();
 
     if (parseInt(this.state.contractor) < 0) {
       this.setState({
-        error: 'Contractor was not selected! Please, choose contractor form the list'
-      })
+        error:
+          "Contractor was not selected! Please, choose contractor form the list",
+      });
     } else {
-      this.props.changeSubmitState()
+      this.props.changeSubmitState();
       this.formData = this.state;
       delete this.formData.error;
-      await foxApi.patchEntityOf('projects', this.props.match.params.id, this.formData)
+      await foxApi
+        .patchEntityOf("projects", this.props.match.params.id, this.formData)
         .then(() => {
-          this.props.history.goBack()
+          this.props.history.goBack();
         })
         .catch((error) => {
-          const errors = handleError(
-            {
-              error: error,
-              operation: "Project update",
-              validationFields: [
-                "name",
-                "location",
-                "description",
-                "start_date",
-                "end_date",
-                "company",
-                "contractor",
-                "work_at_height",
-                "lifting_work",
-                "confined_space",
-                "hot_work",
-                "chemical_handling",
-                "work_alone",
-                "work_at_sensitive_area",
-                "cold_work",
-                "file",
-                "template",
-                "url_to_doc"
-              ]
-            });
+          const errors = handleError({
+            error: error,
+            operation: "Project update",
+            validationFields: [
+              "name",
+              "location",
+              "description",
+              "start_date",
+              "end_date",
+              "company",
+              "contractor",
+              "work_at_height",
+              "lifting_work",
+              "confined_space",
+              "hot_work",
+              "chemical_handling",
+              "work_alone",
+              "work_at_sensitive_area",
+              "cold_work",
+              "file",
+              "template",
+              "url_to_doc",
+            ],
+          });
           this.setState({
-            error: errors
+            error: errors,
           });
         })
-        .finally(() => this.props.changeSubmitState())
+        .finally(() => this.props.changeSubmitState());
     }
-  }
+  };
 
   componentDidMount = async () => {
-    this.props.setProjectId(this.props.match.params.id)
-    await this.props.getProfileFetch()
-      .then(() => foxApi.getDetailsOf('projects', this.props.match.params.id))
+    this.props.setProjectId(this.props.match.params.id);
+    await this.props
+      .getProfileFetch()
+      .then(() => foxApi.getDetailsOf("projects", this.props.match.params.id))
       .then((data) => this.setState({ ...data }))
-      .then(() => this.props.getContractorList({ signal: this.abortController.signal }))
+      .then(() =>
+        this.props.getContractorList({ signal: this.abortController.signal })
+      )
       .catch((error) => console.log(error))
-      .finally(() => this.props.changeLoadingState())
-  }
+      .finally(() => this.props.changeLoadingState());
+  };
 
   componentWillUnmount = async () => {
     this.abortController.abort();
     await this.props.clearList();
-    this.props.setProjectId("")
-  }
+    this.props.setProjectId("");
+  };
 
   abortController = new window.AbortController();
 
   render = () => {
-    const options = this.props.options ? this.props.options.map(option => { return { value: option.id, label: option.username } }) : null
+    console.log(this.state);
+    const options = this.props.options
+      ? this.props.options.map((option) => {
+          return { value: option.id, label: option.username };
+        })
+      : null;
     return (
       <CRow>
-        <CCol>{this.props.role === 'CliAdm' ?
-          <CCard>
-            <CCardHeader>
-              <CCardTitle>Project Details</CCardTitle>
-            </CCardHeader>
-            <CCardBody>
-              <WithLoadingSpinner loading={this.props.loading}>
-                <CForm
-                  onSubmit={this.handleSubmit}
-                >
-                  <DjangoCSRFToken />
-                  <CFormGroup>
-                    <CLabel htmlFor="name">Name</CLabel>
-                    <CInput
-                      id="name"
-                      name='name'
-                      placeholder="Enter project name"
-                      value={this.state.name}
-                      onChange={this.handleChange}
-                      readOnly={this.props.submitting}
-                      disabled={this.props.submitting}
-                      required />
-                  </CFormGroup>
-                  <CFormGroup>
-                    <CLabel htmlFor="location">Location of works</CLabel>
-                    <CInput
-                      id="location "
-                      name="location"
-                      placeholder="Project Works Location"
-                      value={this.state.location}
-                      onChange={this.handleChange}
-                      readOnly={this.props.submitting}
-                      disabled={this.props.submitting}
-
-                      required />
-                  </CFormGroup>
-                  <CFormGroup>
-                    <CLabel htmlFor="description">Description</CLabel>
-                    <CTextarea
-                      id="descrption"
-                      name="description"
-                      placeholder="Please, enter short description of the project"
-                      value={this.state.description}
-                      onChange={this.handleChange}
-                      disabled={this.props.submitting}
-                      readOnly={this.props.submitting}
-                      required
-                    />
-                  </CFormGroup>
-                  <CFormGroup>
-                    <CRow>
-                      <CCol lg="6">
-                        <CLabel htmlFor="start_date">Start Date</CLabel>
-                        <CInput
-                          type="datetime-local"
-                          id="start_date"
-                          name="start_date"
-                          value={this.state.start_date}
-                          onChange={this.handleChange}
-                          disabled={this.props.submitting}
-                          readOnly={this.props.submitting}
-                          required
-                        />
-                      </CCol>
-                      <CCol lg="6">
-                        <CLabel htmlFor="end_date">End Date</CLabel>
-                        <CInput
-                          type="datetime-local"
-                          id="end_date"
-                          name="end_date"
-                          value={this.state.end_date}
-                          onChange={this.handleChange}
-                          disabled={this.props.submitting}
-                          readOnly={this.props.submitting}
-                          required
-                        />
-                      </CCol>
-                    </CRow>
-                  </CFormGroup>
-                  <FoxReactSelectFormGroup
+        <CCol>
+          {this.props.role === "CliAdm" ? (
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Project Details</CCardTitle>
+              </CCardHeader>
+              <CCardBody>
+                <WithLoadingSpinner loading={this.props.loading}>
+                  <CForm onSubmit={this.handleSubmit}>
+                    <DjangoCSRFToken />
+                    <CFormGroup>
+                      <CLabel htmlFor="name">Name</CLabel>
+                      <CInput
+                        id="name"
+                        name="name"
+                        placeholder="Enter project name"
+                        value={this.state.name}
+                        onChange={this.handleChange}
+                        readOnly={this.props.submitting}
+                        disabled={this.props.submitting}
+                        required
+                      />
+                    </CFormGroup>
+                    <CFormGroup>
+                      <CLabel htmlFor="location">Location of works</CLabel>
+                      <CInput
+                        id="location "
+                        name="location"
+                        placeholder="Project Works Location"
+                        value={this.state.location}
+                        onChange={this.handleChange}
+                        readOnly={this.props.submitting}
+                        disabled={this.props.submitting}
+                        required
+                      />
+                    </CFormGroup>
+                    <CFormGroup>
+                      <CLabel htmlFor="description">Description</CLabel>
+                      <CTextarea
+                        id="descrption"
+                        name="description"
+                        placeholder="Please, enter short description of the project"
+                        value={this.state.description}
+                        onChange={this.handleChange}
+                        disabled={this.props.submitting}
+                        readOnly={this.props.submitting}
+                        required
+                      />
+                    </CFormGroup>
+                    <CFormGroup>
+                      <CRow>
+                        <CCol lg="6">
+                          <CLabel htmlFor="start_date">Start Date</CLabel>
+                          <CInput
+                            type="datetime-local"
+                            id="start_date"
+                            name="start_date"
+                            value={this.state.start_date}
+                            onChange={this.handleChange}
+                            disabled={this.props.submitting}
+                            readOnly={this.props.submitting}
+                            required
+                          />
+                        </CCol>
+                        <CCol lg="6">
+                          <CLabel htmlFor="end_date">End Date</CLabel>
+                          <CInput
+                            type="datetime-local"
+                            id="end_date"
+                            name="end_date"
+                            value={this.state.end_date}
+                            onChange={this.handleChange}
+                            disabled={this.props.submitting}
+                            readOnly={this.props.submitting}
+                            required
+                          />
+                        </CCol>
+                      </CRow>
+                    </CFormGroup>
+                    {/* <FoxReactSelectFormGroup
                     options={options}
                     inputInfo="contractor"
                     inputValue={this.state.contractor}
                     handleChange={this.handleReactSelect}
                     disabled={this.props.submitting}
                     readOnly={this.props.submitting}
-                  />
-                  <CFormGroup>
-                    <CLink
-                      className="btn btn-outline-dark mr-3"
-                      to={`${this.props.match.url}/ptw`}
+                  /> */}
+                    <div className="mb-3">
+                      <strong>Contractor: </strong>
+                      {this.props.options
+                        ? this.props.options.filter(
+                            (option) => option.id === this.state.contractor
+                          )[0].username
+                        : ""}
+                    </div>
+                    <CFormGroup>
+                      <CLink
+                        className="btn btn-outline-dark mr-3"
+                        to={`${this.props.match.url}/ptw`}
+                      >
+                        See Permission To Work
+                      </CLink>
+                      <CLink
+                        className="btn btn-outline-dark"
+                        to={`${this.props.match.url}/documents`}
+                      >
+                        Attached Documents
+                      </CLink>
+                    </CFormGroup>
+                    <FoxSwitchGroup
+                      groupLabel="Choose the related hazardous work
+                  from the list below:"
+                      options={permitOptions}
+                      handleCheck={this.handleCheck}
+                      parentState={this.state}
+                      readOnly={this.props.submitting}
+                      disabled={this.props.submitting}
+                    />
+                    <CButton
+                      disabled={this.props.submitting}
+                      shape="pill"
+                      type="submit"
+                      color="dark"
+                      variant="outline"
+                      block
                     >
-                      See Permission To Work
-              </CLink>
-                    <CLink
-                      className="btn btn-outline-dark"
-                      to={`${this.props.match.url}/documents`}
-                    >
-                      Attached Documents
-              </CLink>
-                  </CFormGroup>
-                  <FoxSwitchGroup
-                    groupLabel='Choose the related hazardous work
-                  from the list below:'
-                    options={permitOptions}
-                    handleCheck={this.handleCheck}
-                    parentState={this.state}
-                    readOnly={this.props.submitting}
-                    disabled={this.props.submitting}
-                  />
-                  <CButton disabled={this.props.submitting} shape="pill" type="submit" color="dark" variant="outline" block><SubmitSpinner submitting={this.props.submitting} />Save changes</CButton>
-                  {this.state.error
-                    ? <p className="fox-form-invalid-feedback">{this.state.error}</p>
-                    : null
-                  }
-                </CForm>
-              </WithLoadingSpinner>
-            </CCardBody>
-          </CCard>
-          :
-          <React.Fragment>
-            <ActivityLog projectId={this.props.match.params.id} />
-            <CEmbed
-              ratio="16by9"
-            >
-              <iframe src={`${window.location.origin}/api/ptw/${this.props.match.params.id}`} />
-            </CEmbed>
-          </React.Fragment>
-        }
+                      <SubmitSpinner submitting={this.props.submitting} />
+                      Save changes
+                    </CButton>
+                    {this.state.error ? (
+                      <p className="fox-form-invalid-feedback">
+                        {this.state.error}
+                      </p>
+                    ) : null}
+                  </CForm>
+                </WithLoadingSpinner>
+              </CCardBody>
+            </CCard>
+          ) : (
+            <React.Fragment>
+              <ActivityLog projectId={this.props.match.params.id} />
+              <CEmbed ratio="16by9">
+                <iframe
+                  src={`${window.location.origin}/api/ptw/${this.props.match.params.id}`}
+                />
+              </CEmbed>
+            </React.Fragment>
+          )}
         </CCol>
-      </CRow >
-    )
-  }
+      </CRow>
+    );
+  };
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     company: state.currentUser.company,
     options: state.entityListTable.tableData,
-    role: state.currentUser.role
-  }
-}
+    role: state.currentUser.role,
+  };
+};
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   getProfileFetch: () => dispatch(getProfileFetch()),
-  getContractorList: ({ ...params }) => dispatch(getContractorList({ ...params })),
+  getContractorList: ({ ...params }) =>
+    dispatch(getContractorList({ ...params })),
   setProjectId: (id) => dispatch(setProjectId(id)),
-  clearList: () => dispatch(clearList())
-})
+  clearList: () => dispatch(clearList()),
+});
 
-export default connect(mapStateToProps, mapDispatchToProps)(WithLoading(ProjectDetail))
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(WithLoading(ProjectDetail));
